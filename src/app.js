17d328d1cd73890fa2562707f172eeb761352061
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2020-11-04 17:40:36 -0500

    gradient-css-code-display
    used .innerText to display the css code needed to use the gradient

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+//SELECTORS
+
 const body = document.querySelector(".gradient");
+
 const horizontalOne = document.querySelector(".horizontal-1");
 const horizontalTwo = document.querySelector(".horizontal-2");
 const diagonalRightOne = document.querySelector(".diagonal-right-1");
@@ -8,26 +11,12 @@ const diagonalLeftTwo = document.querySelector(".diagonal-left-2");
 const verticalOne = document.querySelector(".vertical-1");
 const verticalTwo = document.querySelector(".vertical-2");
 
-function setHorizontalGradient() {
-  body.style.background = 
-  `linear-gradient(to right, ${horizontalOne.value}, ${horizontalTwo.value})`;
-}
+const horizontalCSS = document.querySelector(".horizontal-css");
+const diagonalRightCSS = document.querySelector(".diagonal-right-css");
+const diagonalLeftCSS = document.querySelector(".diagonal-left-css");
+const verticalCSS = document.querySelector(".vertical-css");
 
-function setDiagonalRightGradient() {    
-  body.style.background = 
-  `linear-gradient(to bottom right, ${diagonalRightOne.value}, ${diagonalRightTwo.value})`;
-}
-
-
-function setDiagonalLeftGradient() {
-  body.style.background = 
-  `linear-gradient(to bottom left, ${diagonalLeftOne.value}, ${diagonalLeftTwo.value})`;
-}
-
-function setVerticalGradient() {
-  body.style.background = 
-  `linear-gradient(${verticalOne.value}, ${verticalTwo.value})`;
-}
+//EVENT LISTENERS
 
 //HORIZONTAL 
 horizontalOne.addEventListener("input", setHorizontalGradient);
@@ -45,3 +34,33 @@ diagonalLeftTwo.addEventListener("input", setDiagonalLeftGradient);
 //VERTICAL
 verticalOne.addEventListener("input", setVerticalGradient); 
 verticalTwo.addEventListener("input", setVerticalGradient); 
+
+
+//FUNCTIONS
+function setHorizontalGradient() {
+  body.style.background = 
+  `linear-gradient(to right, ${horizontalOne.value}, ${horizontalTwo.value})`;
+
+   horizontalCSS.innerText = `background-image: linear-gradient(to right, ${horizontalOne.value}, ${horizontalTwo.value})`
+}
+
+function setDiagonalRightGradient() {    
+  body.style.background = 
+  `linear-gradient(to bottom right, ${diagonalRightOne.value}, ${diagonalRightTwo.value})`;
+    
+  diagonalRightCSS.innerText = `background-image: linear-gradient(to bottom right, ${diagonalRightOne.value}, ${diagonalRightTwo.value})`;
+}
+
+function setDiagonalLeftGradient() {
+  body.style.background = 
+  `linear-gradient(to bottom left, ${diagonalLeftOne.value}, ${diagonalLeftTwo.value})`;
+
+  diagonalLeftCSS.innerText = `background-image: linear-gradient(to bottom left, ${diagonalLeftOne.value}, ${diagonalLeftTwo.value})`;
+}
+
+function setVerticalGradient() {
+  body.style.background = 
+  `linear-gradient(${verticalOne.value}, ${verticalTwo.value})`;
+
+    verticalCSS.innerText = `background-image: linear-gradient(${verticalOne.value}, ${verticalTwo.value})`;
+}
