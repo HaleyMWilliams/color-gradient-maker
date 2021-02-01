53312310676973fc7f2282ef7b7db55dfbad18b1
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2021-02-01 14:23:48 -0500

    updated-css

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ const diagonalLeftTwo = document.querySelector(".diagonal-left-2");
 const verticalOne = document.querySelector(".vertical-1");
 const verticalTwo = document.querySelector(".vertical-2");
 
+const horizontalBackground = document.querySelector("#horizontal");
+const diagonalRightBackground = document.querySelector("#diagonal-right");
+const diagonalLeftBackground = document.querySelector("#diagonal-left");
+const verticalBackground = document.querySelector("#vertical");
+
 const horizontalCSS = document.querySelector(".horizontal-css");
 const diagonalRightCSS = document.querySelector(".diagonal-right-css");
 const diagonalLeftCSS = document.querySelector(".diagonal-left-css");
@@ -38,29 +43,29 @@ verticalTwo.addEventListener("input", setVerticalGradient);
 
 //FUNCTIONS
 function setHorizontalGradient() {
-  body.style.background = 
+  horizontalBackground.style.background = 
   `linear-gradient(to right, ${horizontalOne.value}, ${horizontalTwo.value})`;
 
-   horizontalCSS.innerText = `background-image: linear-gradient(to right, ${horizontalOne.value}, ${horizontalTwo.value})`
+   horizontalCSS.innerText = `background-image: linear-gradient(to right, ${horizontalOne.value}, ${horizontalTwo.value});`
 }
 
 function setDiagonalRightGradient() {    
-  body.style.background = 
+  diagonalRightBackground.style.background = 
   `linear-gradient(to bottom right, ${diagonalRightOne.value}, ${diagonalRightTwo.value})`;
     
-  diagonalRightCSS.innerText = `background-image: linear-gradient(to bottom right, ${diagonalRightOne.value}, ${diagonalRightTwo.value})`;
+  diagonalRightCSS.innerText = `background-image: linear-gradient(to bottom right, ${diagonalRightOne.value}, ${diagonalRightTwo.value});`;
 }
 
 function setDiagonalLeftGradient() {
-  body.style.background = 
+  diagonalLeftBackground.style.background = 
   `linear-gradient(to bottom left, ${diagonalLeftOne.value}, ${diagonalLeftTwo.value})`;
 
-  diagonalLeftCSS.innerText = `background-image: linear-gradient(to bottom left, ${diagonalLeftOne.value}, ${diagonalLeftTwo.value})`;
+  diagonalLeftCSS.innerText = `background-image: linear-gradient(to bottom left, ${diagonalLeftOne.value}, ${diagonalLeftTwo.value});`;
 }
 
 function setVerticalGradient() {
-  body.style.background = 
+  verticalBackground.style.background = 
   `linear-gradient(${verticalOne.value}, ${verticalTwo.value})`;
 
-    verticalCSS.innerText = `background-image: linear-gradient(${verticalOne.value}, ${verticalTwo.value})`;
+    verticalCSS.innerText = `background-image: linear-gradient(${verticalOne.value}, ${verticalTwo.value});`;
 }
