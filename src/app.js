e4a4ea78140a1a40676bf65c5d3219a5979fc228
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2021-02-02 11:26:31 -0500

    reset-button

--- src/app.js
+++ src/app.js
@@ -21,25 +21,32 @@ const diagonalRightCSS = document.querySelector(".diagonal-right-css");
 const diagonalLeftCSS = document.querySelector(".diagonal-left-css");
 const verticalCSS = document.querySelector(".vertical-css");
 
+const hButton = document.querySelector("#hButton");
+const dLButton = document.querySelector("#dLButton");
+const dRButton = document.querySelector("#dRButton");
+const vButton = document.querySelector("#vButton");
+
 //EVENT LISTENERS
 
 //HORIZONTAL 
 horizontalOne.addEventListener("input", setHorizontalGradient);
 horizontalTwo.addEventListener("input", setHorizontalGradient); 
+hButton.addEventListener("click", resetHorizontalGradient);
 
 // RIGHT DIAGONAL
 diagonalRightOne.addEventListener("input", setDiagonalRightGradient); 
 diagonalRightTwo.addEventListener("input", setDiagonalRightGradient); 
-
+dRButton.addEventListener("click", resetDiagonalRightGradient)
 
 // LEFT DIAGONAL
 diagonalLeftOne.addEventListener("input", setDiagonalLeftGradient); 
 diagonalLeftTwo.addEventListener("input", setDiagonalLeftGradient); 
+dLButton.addEventListener("click", resetDiagonalLeftGradient);
 
 //VERTICAL
 verticalOne.addEventListener("input", setVerticalGradient); 
 verticalTwo.addEventListener("input", setVerticalGradient); 
-
+vButton.addEventListener("click", resetVerticalGradient);
 
 //FUNCTIONS
 function setHorizontalGradient() {
@@ -49,11 +56,21 @@ function setHorizontalGradient() {
    horizontalCSS.innerText = `background-image: linear-gradient(to right, ${horizontalOne.value}, ${horizontalTwo.value});`
 }
 
+function resetHorizontalGradient() {
+  horizontalBackground.style.background = 'white';
+  horizontalCSS.innerText = '';
+}
+
 function setDiagonalRightGradient() {    
   diagonalRightBackground.style.background = 
   `linear-gradient(to bottom right, ${diagonalRightOne.value}, ${diagonalRightTwo.value})`;
     
   diagonalRightCSS.innerText = `background-image: linear-gradient(to bottom right, ${diagonalRightOne.value}, ${diagonalRightTwo.value});`;
+}
+
+function resetDiagonalRightGradient (){
+  diagonalRightBackground.style.background = 'white';
+  diagonalRightCSS.innerText = '';
 }
 
 function setDiagonalLeftGradient() {
@@ -63,9 +80,19 @@ function setDiagonalLeftGradient() {
   diagonalLeftCSS.innerText = `background-image: linear-gradient(to bottom left, ${diagonalLeftOne.value}, ${diagonalLeftTwo.value});`;
 }
 
+function resetDiagonalLeftGradient () {
+  diagonalLeftBackground.style.background = 'white';
+  diagonalLeftCSS.innerText = '';
+}
+
 function setVerticalGradient() {
   verticalBackground.style.background = 
   `linear-gradient(${verticalOne.value}, ${verticalTwo.value})`;
 
     verticalCSS.innerText = `background-image: linear-gradient(${verticalOne.value}, ${verticalTwo.value});`;
+}
+
+function resetVerticalGradient() {
+  verticalBackground.style.background = 'white';
+  verticalCSS.innerText = '';
 }
